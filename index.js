692967615600e26e33bebe6db244b0eec67978b3
yang ouyang <yang.ouyang@blumeglobal.com>
2019-07-29 09:37:19 -0700

    asda

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/', (req, res) => {
+    return res.json({"abc": "cde"});
+})
+
+
+app.get('/api/create', (req, res) => {
+	fs.writeFile("/bob/helloWorld.txt","hello there", ()=>{})
+	return res.json({ success: true })
+});
+
+app.listen(3000)
